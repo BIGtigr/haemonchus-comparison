a0d67e04d74ae615d626b743718222c6e433c89e
Jeff Wintersinger <jeff.git@wintersinger.com>
2014-02-19 12:20:28 -0700

    Change directory structure for InParanoid run.

---
+++ protocols/find-annotation-orthologs/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+OUTDIR=../../runs/inparanoid
+mkdir -p $OUTDIR
+EXTRA_ARGS="--only-analyze"
+
+for matrix in BLOSUM{62,80}; do
+  # Haemonchus against Haemonchus.
+  ./run_inparanoid.py \
+    --matrix $matrix \
+    --outgroup PRJNA13758 \
+    $EXTRA_ARGS \
+    PRJEB506 PRJNA205202 \
+    $OUTDIR &
+  ./run_inparanoid.py \
+    --matrix $matrix \
+    $EXTRA_ARGS \
+    PRJEB506 PRJNA205202 \
+    $OUTDIR &
+
+  # C. elegans against C. briggsae.
+  ./run_inparanoid.py \
+    --matrix $matrix \
+    $EXTRA_ARGS \
+    PRJNA13758 PRJNA10731 \
+    $OUTDIR &
+
+  # Haemonchus against C. elegans.
+  ./run_inparanoid.py \
+    --matrix $matrix \
+    $EXTRA_ARGS \
+    PRJEB506 PRJNA13758 \
+    $OUTDIR &
+  ./run_inparanoid.py \
+    --matrix $matrix \
+    $EXTRA_ARGS \
+    PRJNA205202 PRJNA13758 \
+    $OUTDIR &
+done
+
+wait
--- protocols/find-annotation-orthologs/run_inparanoid.py
+++ protocols/find-annotation-orthologs/run_inparanoid.py
@@ -23,11 +23,11 @@ class CommandManager(object):
 CONF = {}
 cm = CommandManager()
 
-def set_config(run_dir):
+def set_config(parent_dir, run_name):
   global CONF
   CONF['BASEDIR'] = os.path.abspath(os.path.expanduser('~/work/jubilant-peanut'))
   CONF['PROTDIR'] = CONF['BASEDIR'] + '/protocols/find-annotation-orthologs'
-  CONF['RUNDIR']  = os.path.abspath(run_dir)
+  CONF['RUNDIR']  = os.path.abspath(os.path.join(parent_dir, run_name))
 
   CONF['DATASETS'] = {
     'PRJEB506': {
@@ -63,22 +63,24 @@ def create_dirs():
   cm.run('mkdir -p ' + CONF['RUNDIR'])
   os.chdir(CONF['RUNDIR'])
   cm.run('mkdir proteins')
+  cm.run('mkdir results')
 
-def filter_isoforms(genome_id, protein_path, delimiter):
+def filter_isoforms(genome_id, protein_path, delimiter, regenerate_data):
   filtered_file = '{rundir}/proteins/{genome_id}.filtered_isoforms.fa'.format(
     rundir = CONF['RUNDIR'],
     genome_id = genome_id,
   )
-  cm.run('{protdir}/filter_isoforms.py {protein_path} {delimiter} > {filtered_file}'.format(
-    protdir = CONF['PROTDIR'],
-    genome_id = genome_id,
-    protein_path = protein_path,
-    delimiter = delimiter,
-    filtered_file = filtered_file,
-  ))
+  if regenerate_data:
+    cm.run('{protdir}/filter_isoforms.py {protein_path} {delimiter} > {filtered_file}'.format(
+      protdir = CONF['PROTDIR'],
+      genome_id = genome_id,
+      protein_path = protein_path,
+      delimiter = delimiter,
+      filtered_file = filtered_file,
+    ))
   return filtered_file
 
-def alter_fasta_ids(genome_id, protein_path):
+def alter_fasta_ids(genome_id, protein_path, regenerate_data):
   munged_file = '{rundir}/proteins/{genome_id}.munged.fa'.format(
     rundir = CONF['RUNDIR'],
     genome_id = genome_id,
@@ -88,35 +90,34 @@ def alter_fasta_ids(genome_id, protein_path):
     genome_id = genome_id,
   )
 
-  cm.run('cat {protein_path} | {protdir}/munge_fasta.py {genome_id} {munged_file} {name_map}'.format(
-    protein_path = protein_path,
-    protdir = CONF['PROTDIR'],
-    genome_id = genome_id,
-    munged_file = munged_file,
-    name_map = name_map,
-  ))
+  if regenerate_data:
+    cm.run('cat {protein_path} | {protdir}/munge_fasta.py {genome_id} {munged_file} {name_map}'.format(
+      protein_path = protein_path,
+      protdir = CONF['PROTDIR'],
+      genome_id = genome_id,
+      munged_file = munged_file,
+      name_map = name_map,
+    ))
   return {
     'munged_fasta': munged_file,
     'name_map':     name_map
   }
 
-
-def prepare_inparanoid(run_name, outgroup):
+def prepare_inparanoid(outgroup):
   os.chdir(CONF['RUNDIR'])
-  cm.run('cp -a {inparanoid_path} {run_name}'.format(
-    run_name = run_name,
+  cm.run('cp -a {inparanoid_path} inparanoid'.format(
     inparanoid_path = os.path.expanduser('~/.apps/inparanoid/'),
   ))
 
   if outgroup:
-    cm.run('''sed -i 's/^$use_outgroup = 0/$use_outgroup = 1/' {run_name}/inparanoid.pl'''.format(run_name = run_name))
+    cm.run('''sed -i 's/^$use_outgroup = 0/$use_outgroup = 1/' inparanoid/inparanoid.pl''')
   # Use multiple CPUs.
-  cm.run('''sed -i 's/^$blastall = "blastall"/$blastall = "blastall -a28"/' {run_name}/inparanoid.pl'''.format(run_name = run_name))
+  cm.run('''sed -i 's/^$blastall = "blastall"/$blastall = "blastall -a28"/' inparanoid/inparanoid.pl''')
   # Set matrix.
-  cm.run('''sed -i 's/^$matrix = "BLOSUM62"/$matrix = "BLOSUM80"/' {run_name}/inparanoid.pl'''.format(run_name = run_name))
+  cm.run('''sed -i 's/^$matrix = "BLOSUM62"/$matrix = "BLOSUM80"/' inparanoid/inparanoid.pl''')
 
-def run_inparanoid(run_name, genome_a, genome_b, outgroup):
-  os.chdir(os.path.join(CONF['RUNDIR'], run_name))
+def run_inparanoid(genome_a, genome_b, outgroup):
+  os.chdir(os.path.join(CONF['RUNDIR'], 'inparanoid'))
 
   genomes = [genome_a, genome_b]
   if outgroup:
@@ -134,22 +135,21 @@ def run_inparanoid(run_name, genome_a, genome_b, outgroup):
     )}
   )
 
-def process_results(run_name, genome_a, genome_b, munged_fnames):
-  os.chdir(os.path.join(CONF['RUNDIR'], run_name))
-  cm.run('mkdir results')
-  parsed = 'results/inparanoid_results.json'
+def process_results(genome_a, genome_b, munged_fnames):
+  os.chdir(os.path.join(CONF['RUNDIR'], 'results'))
+  parsed = 'inparanoid_results.json'
 
   cm.run('{protdir}/parse_inparanoid.py {inparanoid_output} > {parsed}'.format(
     protdir = CONF['PROTDIR'],
-    inparanoid_output = glob.glob('Output*')[0],
+    inparanoid_output = glob.glob('../inparanoid/Output*')[0],
     parsed = parsed
   ))
-  cm.run('cat {parsed} | {protdir}/summarize_groups.py > results/orthologue-summary'.format(
+  cm.run('cat {parsed} | {protdir}/summarize_groups.py > orthologue-summary'.format(
     parsed = parsed,
     protdir = CONF['PROTDIR']
   ))
   cm.run(("cat {parsed} | {protdir}/plot_group_summary.py '{friendly_name_a}' '{friendly_name_b}' " + \
-    "results/ortho_groups_linear.png results/ortho_groups_log.png").format(
+    "ortho_groups_linear.png ortho_groups_log.png").format(
     parsed = parsed,
     protdir = CONF['PROTDIR'],
     friendly_name_a = CONF['DATASETS'][genome_a]['friendly_name'],
@@ -160,7 +160,7 @@ def process_results(run_name, genome_a, genome_b, munged_fnames):
     '{name_map_b} ' + \
     '{annotation_a} ' + \
     '{annotation_b} ' + \
-    '> results/transcript-groups').format(
+    '> transcript-groups').format(
     parsed = parsed,
     protdir = CONF['PROTDIR'],
     name_map_a = munged_fnames[genome_a]['name_map'],
@@ -169,16 +169,11 @@ def process_results(run_name, genome_a, genome_b, munged_fnames):
     annotation_b = CONF['DATASETS'][genome_b]['annotation'],
   ))
 
-def perform_run(genome_a, genome_b, outgroup, matrix, munged_fnames):
-  run_name = '{a}-{b}.{outgroup}-outgroup.{matrix}'.format(
-    a = genome_a,
-    b = genome_b,
-    outgroup = outgroup or 'no',
-    matrix = matrix,
-  )
-  prepare_inparanoid(run_name, outgroup)
-  run_inparanoid(run_name, genome_a, genome_b, outgroup)
-  process_results(run_name, genome_a, genome_b, munged_fnames)
+def perform_run(genome_a, genome_b, outgroup, munged_fnames, regenerate_data):
+  if regenerate_data:
+    prepare_inparanoid(outgroup)
+    run_inparanoid(genome_a, genome_b, outgroup)
+  process_results(genome_a, genome_b, munged_fnames)
 
 # This could be moved into a different function, as we only need to munge the
 # FASTA files for genomes used in our current run, rather than all specified
@@ -186,16 +181,25 @@ def perform_run(genome_a, genome_b, outgroup, matrix, munged_fnames):
 # to munge from four to three (when an outgroup is used), however, which would
 # reduce the runtime by only a few minutes. Thus, I choose instead to save
 # myself the effort of restructuring this operation.
-def munge_all_fasta_files(datasets):
+def munge_all_fasta_files(datasets, regenerate_data):
   munged = {}
 
   for genome_id, params in datasets.items():
     protein_path = params['proteins']
     if params['filter_isoforms']:
-      protein_path = filter_isoforms(genome_id, protein_path, params['isoform_delimiter'])
-    munged[genome_id] = alter_fasta_ids(genome_id, params['proteins'])
+      protein_path = filter_isoforms(genome_id, protein_path, params['isoform_delimiter'], regenerate_data)
+    munged[genome_id] = alter_fasta_ids(genome_id, protein_path, regenerate_data)
 
   return munged
+
+def generate_run_name(genome_a, genome_b, outgroup, matrix):
+  run_name = '{a}-{b}.{outgroup}-outgroup.{matrix}'.format(
+    a = genome_a,
+    b = genome_b,
+    outgroup = outgroup or 'no',
+    matrix = matrix,
+  )
+  return run_name
 
 def main():
   parser = argparse.ArgumentParser(description='Run InParanoid and parse results.')
@@ -203,15 +207,19 @@ def main():
   parser.add_argument('genome_b',   help='Second genome')
   parser.add_argument('output_dir', help='Output directory')
   parser.add_argument('-o', '--outgroup', dest='outgroup', action='store', help='Outgroup used to break up large orthologous groups')
+  parser.add_argument('-a', '--only-analyze', dest='regenerate_data',
+    action='store_false', help='Do not recreate directories or run InParanoid. Only analyze existing results.')
   parser.add_argument('-m', '--matrix', dest='matrix', action='store',
     choices=('BLOSUM62', 'BLOSUM80'), default='BLOSUM62', help='Scoring matrix')
   args = parser.parse_args()
 
-  set_config(args.output_dir)
-  create_dirs()
-  munged_fnames = munge_all_fasta_files(CONF['DATASETS'])
+  run_name = generate_run_name(args.genome_a, args.genome_b, args.outgroup, args.matrix)
+  set_config(args.output_dir, run_name)
+  if args.regenerate_data:
+    create_dirs()
+  munged_fnames = munge_all_fasta_files(CONF['DATASETS'], args.regenerate_data)
 
-  perform_run(args.genome_a, args.genome_b, args.outgroup, args.matrix, munged_fnames)
+  perform_run(args.genome_a, args.genome_b, args.outgroup, munged_fnames, args.regenerate_data)
 
 if __name__ == '__main__':
   main()
